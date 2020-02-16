f65b471ddc34814ce737fc822e0bda18dbbe2c0f
Zach McQuiston <zachm@xerva.com>
2020-02-16 14:05:11 -0700

    Adding CNN Dependencies

--- CNN.py
+++ CNN.py
@@ -1,8 +1,28 @@
-# Convolutional Neural Network #
+# Convolutional Neural Network w/Snowflake Source POC #
 
+#Snowflake
 import snowflake.connector
+
+#Keras
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten
+from keras.layers import GlobalMaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.text import Tokenizer
+
+#Other
 import os
 import nltk
+import pandas as pd
+import numpy as np
+import re
+from nltk.corpus import stopwords
+from numpy import array
+from sklearn.model_selection import train_test_split
+
 
 PASSWORD = os.getenv('SNOWSQL_PWD')
 WAREHOUSE = os.getenv('SNOWWAREHOUSE')
