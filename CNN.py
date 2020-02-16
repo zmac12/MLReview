001507a3474ebb11f907ed167f4f8f0ae11c9f7c
Zach McQuiston <zachm@xerva.com>
2020-02-16 13:33:41 -0700

    Adding test data and such

--- CNN.py
+++ CNN.py
@@ -7,3 +7,21 @@ PASSWORD = os.getenv('SNOWSQL_PWD')
 WAREHOUSE = os.getenv('SNOWWAREHOUSE')
 ACCOUNT = os.getenv('SNOWACCT')
 USER = os.getenv('SNOWUSER')
+
+con = snowflake.connector.connect(
+    user='zachmcq',
+    password='seQ2nsatio2nal',
+    account='cfa83386.us-east-1',
+    warehouse=WAREHOUSE,
+    schema='PUBLIC',
+    database='CNNPOC'
+)
+
+cur = con.cursor()
+
+sql = 'SELECT * FROM CNNPOC.PUBLIC.LABELLEDREVIEWSNEW'
+
+cur.execute(sql)
+
+df = cur.fetch_pandas_all()
+print(df)
