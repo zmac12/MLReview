f31f4e880369cb6f3aa865f5600a6cec4fb04a8f
Zach McQuiston <zachm@xerva.com>
2020-02-16 12:09:28 -0700

    Adding USER to validate package

--- CNN.py
+++ CNN.py
@@ -4,5 +4,6 @@ import snowflake.connector
 import os
 
 PASSWORD = os.getenv('SNOWSQL_PWD')
-WAREHOUSE = os.getenv('WAREHOUSE')
+WAREHOUSE = os.getenv('SNOWWAREHOUSE')
 ACCOUNT = os.getenv('SNOWACCT')
+USER = os.getenv('SNOWUSER')
